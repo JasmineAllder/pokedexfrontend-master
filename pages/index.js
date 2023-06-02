31820717da994895e6224699251c3fe55fb11088
Jasmine Allder <jasmineallder@gmail.com>
2023-06-02 09:36:10 +0100

    Third Commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import ContactForm from '@/components/ContactForm'
+//import ContactForm from '@/components/ContactForm'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -10,10 +10,10 @@ export default function Home(){
   return (
     <>
     <Head>
-      <title>Code Lancashire</title>
+      <title>Pokedex App</title>
     </Head>
     <div className="max-w-4xl mx-auto p-5">
-      <ContactForm />
+      
     </div>
     </>
   )
